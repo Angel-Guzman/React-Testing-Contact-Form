c96d16aa2e6843979f9ac820081ca17d1544060c
Angelg10 <angelguzman97@gmail.com>
2020-05-07 18:24:31 -0400

    MVP

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,45 +1,23 @@
 import React from "react";
-import { render, fireEvent, getByTestId } from "@testing-library/react";
+import { render, fireEvent, getByTestId, getByDisplayValue } from "@testing-library/react";
 import ContactForm from './ContactForm';
 
 test ('renders form component', () => {
     render(<ContactForm />);
 });
 
-// test ('contact form can be filled in and submitted', () => {
-//     const { getByLabelText, getByText } = render(<ContactForm />);
-
-//     fireEvent.change(getByLabelText(/first name/i), {target: { value: 'Cat' } });
-//     fireEvent.change(getByLabelText(/last name/i), {target: { value: 'Dog' } });
-//     fireEvent.change(getByLabelText(/email/i), {target: { value: 'pet@mail.com' } });
-//     fireEvent.change(getByLabelText(/message/i), {target: { value: 'I love treats!' } });
-
-//     expect(getByLabelText(/first name/i)).toHaveValue('Cat');
-//     expect(getByLabelText(/last name/i)).toHaveValue('Dog');
-//     expect(getByLabelText(/email/i)).toHaveValue('pet@mail.com');
-//     expect(getByLabelText(/message/i)).toHaveValue('I love treats!');
-
-//     fireEvent.click(getByText(/submit/i));
-    
-// })
-
 test ('contact form can be filled in and submitted', () => {
-    const { getByLabelText, getByText, getByTestId, getByRole } = render(<ContactForm />)
+    const { getByLabelText, getByDisplayValue, getByRole } = render(<ContactForm />)
 
-    const fNameInput = getByLabelText(/first name/i)
-    const lNameInput = getByLabelText(/last name/i)
-    const emailInput = getByLabelText(/email/i)
-    const messageInput = getByLabelText(/message/i)
-    const submitBtn = getByRole('button', {name: /submit/i})
+    fireEvent.change(getByLabelText(/first name/i), {target: {value: 'Cat' } })
+    fireEvent.change(getByLabelText(/last name/i), {target: {value: 'Dog' } })
+    fireEvent.change(getByLabelText(/email/i), {target: {value: 'pet@mail.com' } })
+    fireEvent.change(getByLabelText(/message/i), {target: {value: 'I love treats!' } })
 
+    fireEvent.click(getByRole('button', {name: /submit/i}))
 
-    fireEvent.change(fNameInput, {target: {value: 'Cat' } })
-    fireEvent.change(lNameInput, {target: {value: 'Dog' } })
-    fireEvent.change(emailInput, {target: {value: 'pet@mail.com' } })
-    fireEvent.change(messageInput, {target: {value: 'I love treats!' } })
-
-    fireEvent.click(submitBtn)
+    expect(getByDisplayValue(/cat/i)).toBeInTheDocument();
+    expect(getByDisplayValue(/dog/i)).toBeInTheDocument();
+    expect(getByDisplayValue(/pet@mail.com/i)).toBeInTheDocument();
+    expect(getByDisplayValue(/i love treats!/i)).toBeInTheDocument();
 })
-
-
-// data-testid='button'
